24f14c4977bfa34d9f968c5ea707ef8fee412c37
Gregor Berginc <gregor.berginc@xlab.si>
2017-10-18 03:12:51 +0200

    Build with qpid_proton gem
    With the addition of qpid_proton gem into the core Gemfile, this patch
    allows for installation of this gem automatically from the provided
    GitHub repository.
    
    Signed-off-by: Gregor Berginc <gregor.berginc@xlab.si>

--- bin/qpid_install.sh
+++ bin/qpid_install.sh
@@ -13,10 +13,10 @@ ruby_version=`echo $TRAVIS_RUBY_VERSION | egrep -o '[[:digit:]]\.[[:digit:]]'`
 export GEM_HOME=/home/travis/build/ManageIQ/manageiq-providers-nuage/vendor/bundle/ruby/$ruby_version.0
 
 # Install the dev dependencies for building Qpid proton system library.
-sudo apt-get install -y gcc cmake cmake-curses-gui uuid-dev
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y libsasl2-2 libsasl2-dev
-sudo apt-get install -y swig
+apt-get install -y gcc cmake cmake-curses-gui uuid-dev
+apt-get install -y libssl-dev
+apt-get install -y libsasl2-2 libsasl2-dev
+apt-get install -y swig
 
 # Get the latest Qpid Proton source
 cd $HOME/build
@@ -30,13 +30,13 @@ sed -i .bak -e 's/ 0/ 2/' proton-c/bindings/ruby/qpid_proton.gemspec.in
 # Configure the source of Qpid Proton.
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DSYSINSTALL_BINDINGS=OFF
+cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/pkg -DSYSINSTALL_BINDINGS=OFF
 
 # Make system libraries and the gem.
 make all
 
 # Install system libraries
-sudo make install
+make install
 
 # Manually install the Ruby Gem. This will be installed inside $GEM_HOME directory.
-gem install proton-c/bindings/ruby/qpid_proton-0.18.0.gem
+gem install proton-c/bindings/ruby/qpid_proton-0.18.0.gem -- --with-qpid-proton-lib=$HOME/pkg/lib --with-qpid-proton-include=$HOME/pkg/include/
