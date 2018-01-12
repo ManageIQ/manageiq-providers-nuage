87a7333fb420fb7cddd99a51143e4893b496b9a8
Bronagh Sorota <bsorota@redhat.com>
2018-01-12 10:44:13 -0500

    Merge pull request #59 from miha-plesko/add-event-id
    Assign :ems_ref to the event
    (cherry picked from commit 51c638ca53706fdee53694958d007f77cbfe226d)

--- spec/models/manageiq/providers/nuage/network_manager/event_parser_spec.rb
+++ spec/models/manageiq/providers/nuage/network_manager/event_parser_spec.rb
@@ -1,15 +1,46 @@
 describe ManageIQ::Providers::Nuage::NetworkManager::EventParser do
-  context ".event_to_hash" do
-    it "parses vm_ems_ref into event" do
-      ems = FactoryGirl.create(:ems_nuage_network)
-      message = JSON.parse(File.read(File.join(__dir__, "/event_catcher/alarm_delete.json")))
+  before do
+    allow(SecureRandom).to receive(:uuid).and_return('11111111-2222-3333-4444-555555555555')
+  end
 
-      expect(described_class.event_to_hash(message, ems)).to include(
-        :source     => "NUAGE",
-        :vm_ems_ref => nil,
-        :event_type => 'alarm_delete',
-        :ems_id     => ems
-      )
+  let(:ems) { FactoryGirl.create(:ems_nuage_network) }
+
+  context ".event_to_hash" do
+    [
+      {
+        :name     => 'with requestID',
+        :fixture  => '/event_catcher/subnet_create.json',
+        :expected => {
+          :event_type => 'subnet_create',
+          :ems_ref    => '56e69f6e-a1fd-457f-b4c5-844cfd790153'
+        }
+      },
+      {
+        :name     => 'with null requestID',
+        :fixture  => '/event_catcher/alarm_delete.json',
+        :expected => {
+          :event_type => 'alarm_delete',
+          :ems_ref    => 'random-11111111-2222-3333-4444-555555555555'
+        }
+      },
+      {
+        :name     => 'with empty requestID',
+        :fixture  => '/event_catcher/alarm_create.json',
+        :expected => {
+          :event_type => 'alarm_create',
+          :ems_ref    => 'random-11111111-2222-3333-4444-555555555555'
+        }
+      }
+    ].each do |example|
+      it example[:name] do
+        message = JSON.parse(File.read(File.join(__dir__, example[:fixture])))
+        example[:expected][:ems_id] = ems.id
+        example[:expected][:vm_ems_ref] = nil
+        example[:expected][:source] = 'NUAGE'
+        example[:expected][:message] = example[:expected][:event_type] if example[:expected][:event_type]
+        example[:expected][:full_data] = message.to_hash
+        expect(described_class.event_to_hash(message, ems.id)).to include(example[:expected])
+      end
     end
   end
 end
