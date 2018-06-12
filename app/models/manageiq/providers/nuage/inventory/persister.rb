b775c8f9053b7b9808f40f387aa6fdde31662904
Martin Slemr <mslemr@redhat.com>
2018-06-12 17:03:05 +0200

    Added Parent property to ICs
    Moved from core

--- app/models/manageiq/providers/nuage/inventory/persister.rb
+++ app/models/manageiq/providers/nuage/inventory/persister.rb
@@ -8,8 +8,14 @@ class ManageIQ::Providers::Nuage::Inventory::Persister < ManagerRefresh::Invento
     nil
   end
 
+  def parent
+    manager.presence
+  end
+
+  # Shared properties for inventory collections
   def shared_options
     {
+      :parent   => parent,
       :strategy => strategy,
       :targeted => targeted?,
     }
