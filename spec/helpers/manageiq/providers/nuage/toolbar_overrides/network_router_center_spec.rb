c492e75a282e4b408645ff887cb074dac8cad51b
Miha Pleško <miha.plesko@xlab.si>
2018-09-05 16:29:48 +0200

    Improve coverage: event catcher and custom toolbar
    With this commit we provide some additional unit tests to make
    sure as much code gets executed upon unit testing as possible.
    Namely:
    
    - tests for event catcher (mocking the qpid gem)
    - tests for our custom center-menu button
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

---
+++ spec/helpers/manageiq/providers/nuage/toolbar_overrides/network_router_center_spec.rb
@@ -0,0 +1,16 @@
+describe ManageIQ::Providers::Nuage::ToolbarOverrides::NetworkRouterCenter do
+  let(:button_group)  { described_class.definition['nuage_network_router'] }
+  let(:group_items)   { button_group.buttons }
+  let(:dropdown_edit) { group_items.first }
+
+  it 'assert button group' do
+    expect(button_group).not_to be_nil
+    expect(group_items.size).to eq(1)
+  end
+
+  it 'assert Edit drop-down' do
+    expect(dropdown_edit[:title]).to eq(N_('Edit'))
+    expect(dropdown_edit[:items].size).to eq(1)
+    expect(dropdown_edit[:items][0][:title]).to eq(N_('Create L3 Cloud Subnet'))
+  end
+end
