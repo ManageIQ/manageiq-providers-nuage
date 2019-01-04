6a510e94a74da9da56fc13936ca5462e0fd0907e
Adam Grare <agrare@redhat.com>
2019-01-04 15:28:52 -0500

    Remove duplicated persister shared_options
    The shared_options were duplicated from the base persister

--- app/models/manageiq/providers/nuage/inventory/persister.rb
+++ app/models/manageiq/providers/nuage/inventory/persister.rb
@@ -1,23 +1,4 @@
 class ManageIQ::Providers::Nuage::Inventory::Persister < ManageIQ::Providers::Inventory::Persister
   require_nested :NetworkManager
   require_nested :TargetCollection
-
-  protected
-
-  def strategy
-    nil
-  end
-
-  def parent
-    manager.presence
-  end
-
-  # Shared properties for inventory collections
-  def shared_options
-    {
-      :parent   => parent,
-      :strategy => strategy,
-      :targeted => targeted?,
-    }
-  end
 end
